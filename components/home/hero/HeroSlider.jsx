921e6cb123461b7b0773aba7ad0225435dace830
amr ibrahim <amramr3434@gmail.com>
2026-05-27 17:50:25 +0300

    design: hero

--- components/home/hero/HeroSlider.jsx
+++ components/home/hero/HeroSlider.jsx
@@ -50,23 +50,23 @@ export default function HeroSlider({ brand }) {
           <SwiperSlide key={idx}>
             <div className="relative w-full h-full">
               <img src={s.img} alt={`${s.title}`} className="w-full h-full object-cover" />
-              {/* <div className="absolute inset-0 bg-black/40" /> */}
+              <div className="absolute inset-0 bg-black/40" />
               <div className="absolute inset-0 flex items-end md:items-center">
                 <div className="px-4 md:px-8 lg:px-12 pb-10 md:pb-0 w-full max-w-7xl mx-auto">
                   <div className="max-w-xl text-white">
-                    <h1 className="text-3xl md:text-5xl font-black leading-tight">{s.title}</h1>
-                    <p className="mt-2 text-white/90 text-lg md:text-xl">{s.sub}</p>
+                    <h1 className="lg:text-3xl md:text-5xl font-black leading-tight">{s.title}</h1>
+                    <p className="mt-2 text-white/90 text-sm md:text-xl">{s.sub}</p>
                     <div className="mt-5 flex gap-3">
                       <Link
                         href={buildUrlWithParams('/catalog', getCtaFilters(s.cta1))}
-                        className="rounded-2xl px-5 py-3 font-semibold text-white"
+                        className="rounded-lg text-xs lg:text-lg px-2 py-2 self-center  lg:px-5 lg:py-3 lg:font-semibold text-white"
                         style={{ background: brand.primary }}
                       >
                         {s.cta1}
                       </Link>
                       <Link
                         href={buildUrlWithParams('/catalog', getCtaFilters(s.cta2))}
-                        className="rounded-2xl border px-5 py-3 font-semibold text-white/90 border-white/70"
+                        className="rounded-lg text-xs lg:text-lg px-2 py-2  border lg:px-5 lg:py-3 lg:font-semibold text-white/90 border-white/70"
                       >
                         {s.cta2}
                       </Link>
